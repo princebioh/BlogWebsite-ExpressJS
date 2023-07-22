2cebd3f8f41a35a1edfb36c5119595786f43b467
Prince Bioh <princebioh15@gmail.com>
2023-07-22 12:23:49 +0000

    Lodash Module Added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const ejs = require("ejs"); 
+const _ = require("lodash");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
@@ -12,8 +13,7 @@ const homeContent = "Lorem Ipsum is simply dummy text of the printing and typese
 const aboutContent = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 const contactContent = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.";
 
-app.listen(3000, () => {
-    
+app.listen(3000, () => {   
     console.log("Server Started on port 3000");
 });
 
@@ -42,4 +42,22 @@ app.route("/compose")
         };
         blogPosts.push(userPost);
         res.redirect("/");
-    })
+    });
+
+
+app.route("/posts/:postTitle")
+    .get((req,res) => { 
+        urlParam = _.lowerCase(req.params.postTitle);
+
+        blogPosts.forEach((post) => {     
+            if(_.lowerCase(post.title) === urlParam){
+                res.render("posts",{postContent : post});   
+            }
+            else{
+                console.log("No Match");
+                res.send("Post Not Found!")
+            }
+        });
+    });
+
+
