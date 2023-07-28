cd7b23765133c598ddbeb2eb4a6e59880a658bdf
Prince Bioh <princebioh15@gmail.com>
2023-07-28 23:56:38 +0000

    BlogPost DB created in MongoDB

---
+++ BlogPost.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/todoAppDB");
+
+const postSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : [true, "Tile Not Added"]
+    },
+    body : {
+        type : String,
+        required : true,
+    }
+});
+
+module.exports = mongoose.model("Post", postSchema);
